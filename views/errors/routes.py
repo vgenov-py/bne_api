018c7f81d9c2aee0be49ec16d381be78ac27744a
Vito Genovese <genovese.work@protonmail.com>
2023-06-30 15:23:44 +0200

    forgot

--- views/errors/routes.py
+++ views/errors/routes.py
@@ -13,8 +13,10 @@ def error_404(error):
 
 @errors.app_errorhandler(403)
 def error_403(error):
+    write_error(error)
     return render_template("errors/404.html"), 403
 
 @errors.app_errorhandler(500)
 def error_500(error):
+    write_error(error)
     return render_template("errors/404.html"), 500
